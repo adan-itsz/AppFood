71b7730b8b9d0239930ae29755177bafcf15f141
Adan De Santiago Avalos <adan1995a@gmail.com>
2016-09-03 18:48:46 -0500

    naviagton con listener en dos items del drawer y con boton hacia atras

---
+++ app/src/main/java/com/example/adan/aplicacioncomida/ActivityVentas.java
@@ -0,0 +1,14 @@
+package com.example.adan.aplicacioncomida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityVentas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_ventas);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
